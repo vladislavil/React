7caa599b1da4e41e627d342481fc24e1c0ed3b64
valdislav i <vladoss_19995@mail.ru>
2019-02-12 10:39:29 +0300

    hello world

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Article from './Article'
-import {articles} from './fixtures'
 
-render(<Article article = {articles[0]} />, document.getElementById('container'))
+function HelloWorld() {
+    return <h1>Hello World</h1>
+}
+
+render(<HelloWorld/>, document.getElementById("container"));
