cffd1523758044107a3aeda359439bd009284f40
valdislav i <vladoss_19995@mail.ru>
2019-02-12 12:01:40 +0300

    Articles

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,46 @@
 import React, {Component} from 'react'
 
 export default class Article extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            isOpen: false
+        };
+
+        this.toggleOpen = this.toggleOpen.bind(this);
+    }
+    render() {
+        const {article} = this.props;
+        const {isOpen} = this.state;
+        return(
+          <div>
+            <h3>{article.title}</h3>
+            <button onClick = {this.toggleOpen}> {isOpen ? 'close' : 'open'} </button>
+            {this.getBody()}
+          </div>
+        );
+    }
+
+    getBody() {
+        if(!this.state.isOpen) return null;
+        const {article} = this.props;
+        return <section>{article.text}</section>
+    }
+
+    toggleOpen() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 }
+
+// export default function Article(props) {
+//     const {article} = props;
+//     return(
+//       <div>
+//         <h3>{article.title}</h3>
+//         <section>{article.text}</section>
+//       </div>
+//     );
+// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
+import Article from "./Article"
+import {articles} from "./fixtures"
 
-function HelloWorld() {
-    return <h1>Hello World</h1>
-}
-
-render(<HelloWorld/>, document.getElementById("container"));
+render(<Article article = {articles[0]} />, document.getElementById("container"));
